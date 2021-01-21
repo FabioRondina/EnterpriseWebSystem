1c2f39a51f144d116e1992784952483cf14a57b6
fabio <f.rondina@rgu.ac.uk>
2021-01-21 21:34:38 +0100

    set the port as in the VM

--- main_html.js
+++ main_html.js
@@ -9,6 +9,6 @@ fs.createReadStream('HelloWorld.html').pipe(res)
 
 }) 
 
-server.listen(process.env.PORT || 3000) 
+server.listen(process.env.PORT || 8000) 
 
-console.log('Server running at http://127.0.0.1:3000/');
+console.log('Server running at http://127.0.0.1:8000/');
